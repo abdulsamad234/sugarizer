8c74f98c6948ec0d06066b87c2edd462f2678ca0
abdulsamad aliyu <abdulsamadaliyu17@gmail.com>
2017-11-21 20:39:24 +0200

    Added two player functionality as a boolean value

--- activities/pingpong.activity/js/script.js
+++ activities/pingpong.activity/js/script.js
@@ -5,6 +5,7 @@ var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame
 var canvas = document.getElementById("main-canvas");
 var playerScore = 0;
 var computerScore = 0;
+var playerMode = false;
 
 var width = window.innerWidth;
 var height = window.innerHeight - 58;
@@ -13,7 +14,7 @@ canvas.height = height;
 var context = canvas.getContext('2d');
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(200, 300);
+var ball = new Ball(width/2, height/2);
 var keysDown = {};
 var audio = new Audio('sounds/clang.mp3');
 
@@ -91,11 +92,11 @@ function Player() {
 Player.prototype.update = function(){
   for(var key in keysDown){
     var value = Number(key);
-    if(value == 37){
-      this.paddle.move(-4, 0);
+    if(value == 38){
+      this.paddle.move(0, -8);
     }
-    else if(value == 39){
-      this.paddle.move(4, 0);
+    else if(value == 40){
+      this.paddle.move(0, 8);
     }
     else{
       this.paddle.move(0, 0);
@@ -108,13 +109,13 @@ Paddle.prototype.move = function(x, y){
   this.y += y;
   this.x_speed = x;
   this.y_speed = y;
-  if(this.x < 0){
-    this.x = 0;
-    this.x_speed = 0
+  if(this.y < 0){
+    this.y = 0;
+    this.y_speed = 0
   }
-  else if(this.x + this.width > 400){
-    this.x = 400 - this.width;
-    this.x_speed = 0;
+  else if(this.y + this.height > height){
+    this.y = height - this.height;
+    this.y_speed = 0;
   }
 }
 
@@ -123,23 +124,40 @@ function Computer(){
 }
 
 Computer.prototype.update = function(ball) {
-  var x_pos = ball.x;
-  var diff = -((this.paddle.x + (this.paddle.width/2)) - x_pos) / 2;
-	if(ball.x < 300){
-	  if(diff < 0 && diff < -3){
-	    diff = -1;
-	  }
-	  else if(diff > 0 && diff > 3){
-	    diff = 1;
-	  }
-	  this.paddle.move(diff, 0);
-	  if(this.paddle.x < 0){
-	    this.paddle.x = 0;
-	  }
-	  else if(this.paddle.x + this.paddle.width > 400){
-	    this.paddle.x = 400 - this.paddle.width;
+	if(playerMode == true){
+		for(var key in keysDown){
+	    var value = Number(key);
+	    if(value == 87){
+	      this.paddle.move(0, -8);
+	    }
+	    else if(value == 83){
+	      this.paddle.move(0, 8);
+	    }
+	    else{
+	      this.paddle.move(0, 0);
+	    }
 	  }
 	}
+	else{
+		var y_pos = ball.y;
+	  var diff = -((this.paddle.y + (this.paddle.height)) - y_pos);
+		if(ball.x < width/2){
+		  if(diff < 0 && diff < -7){
+		    diff = -8;
+		  }
+		  else if(diff > 0 && diff > 7){
+		    diff = 8;
+		  }
+		  this.paddle.move(0, diff);
+		  if(this.paddle.y < 0){
+		    this.paddle.y = 0;
+		  }
+		  else if(this.paddle.y > height + 100){
+		    this.paddle.y = height ;
+		  }
+		}
+	}
+  
 };
 
 Player.prototype.render = function(){
@@ -153,8 +171,8 @@ Computer.prototype.render = function(){
 function Ball(x, y){
   this.x = x;
   this.y = y;
-  this.x_speed = 3;
-  this.y_speed = 0;
+  this.x_speed = 7;
+  this.y_speed = 2;
   this.radius = 7;
 }
 
@@ -167,16 +185,16 @@ Ball.prototype.render = function(){
 Ball.prototype.update = function(paddle1, paddle2){
   this.x += this.x_speed;
   this.y += this.y_speed;
-  var top_x = this.x - 5;
-  var top_y = this.y - 5;
-  var bottom_x = this.x + 5;
-  var bottom_y = this.y + 5;
-  if(this.y - 5 < 0){
+  var top_x = this.x - 7;
+  var top_y = this.y - 7;
+  var bottom_x = this.x + 7;
+  var bottom_y = this.y + 7;
+  if(this.y - 7 < 0){
 		playAudio();
     this.y = 5;
     this.y_speed = -this.y_speed;
   }
-  else if(this.y + 5 > height){
+  else if(this.y + 7 > height){
 		playAudio();
     this.y = height - 5;
     this.y_speed = -this.y_speed;
@@ -186,13 +204,15 @@ Ball.prototype.update = function(paddle1, paddle2){
 		if(this.x < 0){
 			playerScore += 1;
 			// document.getElementById("player-score").innerHTML = playerScore;
+			// player won
+			this.x_speed = -7;
 		}
 		else if(this.x > width){
 			computerScore +=1;
 			// document.getElementById("comp-score").innerHTML = computerScore;
+			this.x_speed = 7;
 		}
-    this.y_speed = 0;
-    this.x_speed = 3;
+    this.y_speed = -2;
     this.x = width/2 - 5;
     this.y = height/2 - 5;
 
@@ -201,16 +221,16 @@ Ball.prototype.update = function(paddle1, paddle2){
   if(top_x > width/2){
     if(top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x && top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y){
 			playAudio();
-      this.x_speed = -3;
-      this.y_speed += (paddle1.y_speed / 2);
+      this.x_speed = -7;
+      this.y_speed += (paddle1.y_speed);
       this.x += this.x_speed;
     }
   }
   else{
-    if(top_y < (paddle2.x + paddle2.width) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y){
+    if(top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y){
 			playAudio();
-      this.x_speed = 3;
-      this.y_speed += (paddle2.y_speed / 2);
+      this.x_speed = 7;
+      this.y_speed += (paddle2.y_speed);
       this.x += this.x_speed;
     }
   }
